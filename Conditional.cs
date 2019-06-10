7c0591bc2f6cee37d13e151798af3350580d0f12
Nilot352 <nilotpalbhuyan352@tutanota.com>
2019-06-10 17:59:00 +0530

    Initialized Repository

---
+++ Conditional.cs
@@ -0,0 +1,15 @@
+using System;
+public class Addition
+{
+		public static void Main()
+		{
+				int first_no=2;
+				int second_no=2;
+				int result = first_no+second_no;
+				if(result > 10)
+				 Console.WriteLine($"{result} is greater than 10");
+                else
+                 Console.WriteLine($"{result} is less than 10");
+				 
+		}
+}
---
+++ String_Length.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+public class String_Length
+	{
+
+		public static void Main(string[] args)
+		{
+            string My_Name;
+            My_Name = "Herbert Schildt";
+            Console.WriteLine($"{My_Name} is not important");
+		}
+	}
